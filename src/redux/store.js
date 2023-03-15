d70b4f9e3c4e025ece4b2e473fa67125e7435870
Dorian Gruselin <dorian.gruselin@hotmail.com>
2023-03-15 22:15:46 +0100

    Removed unused stuff

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,6 @@ import storage from 'redux-persist/lib/storage';
 import {
     persistStore,
     persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
 } from 'redux-persist'
 
 
